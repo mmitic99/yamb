7eeeab90f8b0496846d49d9c8e85ebdfc31b3fb9
Mihajlo Mitic <mihajlomiticmiki@gmail.com>
2024-06-08 12:54:20 +0200

    feat: add material ui

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import { Button } from '@mui/material';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         YAMB
+
+        <Button variant='contained'>YAMB</Button>
       </header>
     </div>
   );
 }
 
 export default App;
+ 
